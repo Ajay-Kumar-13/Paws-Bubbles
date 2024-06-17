e847b99ed5078578ced89dbb029c5ec60b159783
Ajay-Kumar-13 <apulleti@gitam.in>
2024-06-17 07:24:33 +0000

    fix css issue

--- src/components/About.js
+++ src/components/About.js
@@ -17,7 +17,7 @@ function About() {
             </div>
             <div className="alignBottom bg-orange jumbotronBottom">
                 <img src="/assests/goldenretriver.png" />
-                <div className="container pt-5 ralewayFont">
+                <div className="container py-5 ralewayFont">
                     <h1>We take care of your pets.</h1>
                     <h5 className="mt-4">Pet spas offer a range of services designed to pamper and care for our furry friends. Whether it's a relaxing massage or a refreshing facial, pet spas provide an opportunity for pets to enjoy some well-deserved pampering while helping to improve their overall health and well-being.</h5>
                 </div>
